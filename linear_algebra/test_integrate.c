50cf7d697970706b480e2dad6b703b2547745f66
Jeff Lynn <jhl1002@wildcats.unh.edu>
2019-03-25 21:19:17 -0400

    Updated integrate to use openMP. Code is wrong!

--- linear_algebra/test_integrate.c
+++ linear_algebra/test_integrate.c
@@ -1,6 +1,7 @@
-
+#include "omp.h"
 #include <stdio.h>
 #include <math.h>
+#include "linear_algebra.h"
 
 static double
 f(double x)
@@ -14,13 +15,22 @@ main(int argc, char **argv)
   const int N = 1000;
   double dx = 1. / N;
 
+  double tbeg = Wtime();
   double sum = 0.;
-  for (int i = 0; i < N; i++) {
-    double x0 = i * dx;
-    double x1 = (i+1) * dx;
-    sum += .5 * (f(x0) + f(x1)) * dx;
+#pragma omp parallel
+  {
+	double local_sum = 0.;
+#pragma omp for
+    for (int i = 0; i < N; i++) {
+      double x0 = i * dx;
+      double x1 = (i+1) * dx;
+      local_sum += .5 * (f(x0) + f(x1)) * dx;;
+    }
+    sum += local_sum;
   }
-  printf("sum = %g\n", sum);
+  double tend = Wtime();
+
+  printf("The sum is %f and the time is %f\n", sum, tend - tbeg);
 
   return 0;
 }
